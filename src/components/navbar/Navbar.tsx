e70ca1126c820ba059f54846897f59b2643b42c6
ornob90 <oornob49@gmail.com>
2025-07-22 18:30:22 +0600

    Add: Navbar with title and Avatar Logo

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { TbBrandBooking } from "react-icons/tb";
+import Avatar from "../shared/Avatar";
+
+const Navbar = () => {
+  return (
+    <section className="flex  justify-between  items-center py-2 border-b px-[3%] border-b-gray-200">
+      {/* Left  */}
+      <section className="flex items-center gap-x-2 text-3xl">
+        <TbBrandBooking className=" text-4xl " />
+        Booking
+      </section>
+
+      <Avatar />
+    </section>
+  );
+};
+
+export default Navbar;
---
+++ src/components/shared/Avatar.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Avatar = () => {
+  return (
+    <div className=" size-[60px] rounded-full border-2 border-gray-300 flex justify-center items-center"> 
+        <Image
+      src={"/smiling-young-man-illustration_1308-174669.avif"}
+      alt={"Avatar"}
+      width={50}
+      height={50}
+      className=" max-w-full max-h-full rounded-full  object-cover "
+    />
+    </div>
+  );
+};
+
+export default Avatar;
